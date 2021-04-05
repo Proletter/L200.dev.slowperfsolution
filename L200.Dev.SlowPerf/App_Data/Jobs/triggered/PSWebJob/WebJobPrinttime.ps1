7937cbbf248469ba3e73e898b05c21202ea54b1e
Oluwaseun <Oluseun.ismaila@gmail.com>
2021-04-05 19:19:46 +0000

    fixed

---
+++ L200.Dev.SlowPerf/App_Data/Jobs/triggered/PSWebJob/WebJobPrinttime.ps1
@@ -0,0 +1,1 @@
+get-Date -format r
---
+++ L200.Dev.SlowPerf/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace L200.Dev.SlowPerf.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult SlowPerf()
+        {
+            System.Threading.Thread.Sleep(30 * 1000);
+            return View();
+        }
+
+        public ActionResult Crash()
+        {
+            throw new Exception("code is HelloWorld");
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult CurrentUser()
+        {
+            string name = "no user found";
+            try
+            {
+                name = this.HttpContext.User.Identity.Name;
+            }
+            catch(Exception )
+            {
+
+            }
+            ViewBag.UserName = name;
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
